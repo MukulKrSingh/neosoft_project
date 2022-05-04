4560952b44da42a9b13b31aa8bb46a97c6c019a5
MukulSingh101 <singhmukul96@gmail.com>
2022-05-04 23:09:34 +0530

    main file comment removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-// ignore: unused_import
-import 'package:neosoft_project/Services/api_services.dart';
-
 import 'app.dart';
 
 void main() {
-  // ApiServices api = ApiServices();
-  // api.getUsers();
+  
   runApp(const App());
 }
